f6a67ba8cf2720c5882410e0b3a305f48c0d0a09
umlx5h <umlx5h21@protonmail.com>
2023-09-28 20:51:40 +0900

    nvim: add nvim config switcher

--- dot_zshrc.d/function.zsh
+++ dot_zshrc.d/function.zsh
@@ -65,3 +65,24 @@ function c() {
     local cmd=$(compgen -c | grep -v '^_' | sort | uniq | fzf)
     [ -n "$cmd" ] && print -z -- "$cmd"
 }
+
+alias vim-astro="NVIM_APPNAME=nvim_astro nvim"
+alias vim-lazy="NVIM_APPNAME=nvim_lazy nvim"
+alias vim-chad="NVIM_APPNAME=nvim_nvchad nvim"
+alias vim-kickstart="NVIM_APPNAME=nvim_kickstart nvim"
+alias vim-tiny="NVIM_APPNAME=nvim_tiny nvim"
+alias vim-default="NVIM_APPNAME=nvim_default nvim"
+
+function vims() {
+    items=("my" "default" "nvim_astro" "nvim_lazy" "nvim_kickstart" "nvim_nvchad" "nvim_tiny")
+    config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    if [[ -z $config ]]; then
+        echo "Nothing selected"
+        return 0
+    elif [[ $config == "my" ]]; then
+        config=""
+    fi
+    NVIM_APPNAME=$config nvim $@
+}
+
+
